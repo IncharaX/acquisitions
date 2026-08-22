0c74e62f44184410dee39e2a8d1d966239d6f3ff
Inchara nk <incharank1@gmail.com>
2026-08-22 19:12:00 +0530

    drizzle

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true });
+
 app.get('/', (req, res) => {
+  logger.info('Hello from acquisitions');
   res.status(200).send('Hello from acquisitions');
+
 });
 
 export default app;
