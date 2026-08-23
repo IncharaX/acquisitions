650250787938544db1922fcb0d131ad487f71144
Inchara nk <incharank1@gmail.com>
2026-08-23 12:39:14 +0530

    worked with op

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,12 +1,12 @@
-const winston from 'winston';
+import winston from 'winston';
 
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
-  format: winston.format.combine((
+  format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.errors({ stack: true }),
     winston.format.json()
-  )),
+  ),
   defaultMeta: { service: 'acquisition-api' },
   transports: [
    
@@ -19,10 +19,10 @@ const logger = winston.createLogger({
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple(),
+      winston.format.colorize(),
+      winston.format.simple(),
     )
-  }));
+  }));  
 }
 
 export default logger;
