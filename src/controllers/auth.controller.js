cd1dd99ec393e6c7532986dde925d17ca1777588
Inchara nk <incharank1@gmail.com>
2026-08-25 15:50:43 +0530

    signup auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,11 @@
 import { signupSchema } from '#validations/auth.validation.js';
 import { formatValidationError } from '#utils/format.js';
 import logger from '#config/logger.js';
+import { createUser } from '#services/auth.service.js';
+import jwttoken from '#utils/jwt.js';
+import cookies from '#utils/cookies.js';
+import { hashPassword } from '#services/auth.service.js';
+
 
 export const signup = async (req, res, next) => {
   try {
@@ -15,13 +20,20 @@ export const signup = async (req, res, next) => {
 
     const { name, email, role } = validationResult.data;
 
-    // AUTH SERVICE
+    const password_hash = await hashPassword(validationResult.data.password);
+
+    const user = await createUser({ name, email, password: password_hash, role });
+
+    const token=jwttoken.sign({ id: user.id, email: user.email, role: user.role });
+
+    cookies.set(res,'token',token);
+    
 
     logger.info(`User registered successfully: ${email}`);
     res.status(201).json({
       message: 'User registered',
       user: {
-        id: 1,name,email,role
+        id: user.id,name: user.name,email: user.email,role: user.role
       },
     });
   } catch (e) {
