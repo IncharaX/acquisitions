b943ff2de3737ae260d9f7da03c535eb73343c7c
Inchara nk <incharank1@gmail.com>
2026-09-04 16:54:24 +0530

    implementes routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import { signup } from '#controllers/auth.controller.js';
 
-const router = express.Router();
-
-router.post('/sign-up', signup);
 
 router.post('/sign-in', (req,res)=>{
   res.send('POST /api/auth/sign-in response');
